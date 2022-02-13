ccb7988067195bbec0b669ffebb3cb996e3949ff
Isarel Getu <israelgetu101@gmail.com>
2022-02-12 21:24:23 -0600

    Done

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,5 +1,12 @@
-let x = 1
-let y = 2
+let x = 1;
+let y = 2;
 
 // Write your code below this line.
 
+if (x > y) {
+  console.log("The max is " + x);
+} else if (x === y) {
+  console.log("They are equal!");
+} else {
+  console.log("The max is " + y);
+}
